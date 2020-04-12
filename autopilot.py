a040c2d3f6ec541d9ea4b168ababbf4cf6c3c29a
Thomas White <tawhite@iinet.net.au>
2020-04-12 18:03:07 +0800

    fix latest release might not be declared

--- autopilot.py
+++ autopilot.py
@@ -13,17 +13,17 @@ def update():
     data = response.json()
     try:
         latest_release = data[0]['tag_name']
+        if latest_release and latest_release != RELEASE:
+            message = "There is a new version of EDAutopilot available!\nWould you like to go to the release download page?"
+            root = tk.Tk()
+            root.withdraw()
+            root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file=resource_path('src/logo.png')))
+            go_to_update = messagebox.askyesno("ED - Autopilot Update", message)
+            if go_to_update:
+                webbrowser.open_new(data[0]['html_url'])
+                return True
     except Exception as e:
         print(e)
-    if latest_release and latest_release != RELEASE:
-        message = "There is a new version of EDAutopilot available!\nWould you like to go to the release download page?"
-        root = tk.Tk()
-        root.withdraw()
-        root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file=resource_path('src/logo.png')))
-        go_to_update = messagebox.askyesno("ED - Autopilot Update", message)
-        if go_to_update:
-            webbrowser.open_new(data[0]['html_url'])
-            return True
     return False
 
 if __name__ == '__main__':
